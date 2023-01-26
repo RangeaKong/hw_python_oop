0b0a3e75399b0f3d6e0bcca370db4b1ff2136a84
Павел Бронников <voidkong@vk.com>
2023-01-26 16:34:52 +0300

    Completed

--- homework.py
+++ homework.py
@@ -11,9 +11,9 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
-    message = ('Тип тренировки {training_type}; Длительность: {duration:.3f} '
-               'Дистанция: {distance:.3f} км; Ср. скорость: {speed:.3f} км/ч;'
-               'Потрачено ккал: {calories:.3f}. '
+    message = ('Тип тренировки: {training_type}; Длительность: {duration:.3f} ч.; '
+               'Дистанция: {distance:.3f} км; Ср. скорость: {speed:.3f} км/ч; '
+               'Потрачено ккал: {calories:.3f}.'
                )
 
     def get_message(self) -> str:
